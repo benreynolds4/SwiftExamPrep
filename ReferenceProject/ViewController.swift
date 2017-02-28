d20106566d3edba123da0b7797d682ccf0380d54
Ben Reynolds <ben.reynolds@ucdconnect.ie>
2017-02-28 14:22:39 +0000

    Initial Commit

---
+++ ReferenceProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReferenceProject
+//
+//  Created by Ben Reynolds on 28/02/2017.
+//  Copyright © 2017 Ben Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
